ceeb7cb287fa674cefe6b2c88bfb93daf85a1672
Amina <minassouane@gmail.com>
2026-03-09 00:40:57 +0100

    feat(posts): connect controller to prisma queries

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,23 @@
-function getPosts() {}
-function postPost() {}
+const db = require("../prisma/queries");
+
+async function getPosts(req, res) {
+  try {
+    const posts = await db.getPosts();
+    res.json(posts);
+  } catch (error) {
+    console.error("Could not get posts : ", error);
+    res.status(500).json({ error: "Failed to fetch posts" });
+  }
+}
+
+async function postPost(req, res) {
+  try {
+    const post = await db.postPost(req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    console.error("Could not create post : ", error);
+    res.status(500).json({ error: "Failed to create post" });
+  }
+}
 
 module.exports = { getPosts, postPost };
