3e7b2916744b4134568735577e8f042be59e3310
Amina <minassouane@gmail.com>
2026-03-09 16:32:31 +0100

    fix(express): add urlencoded middleware for request body parsing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const postsRouter = require("./routes/postsRouter");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 app.use("/login", authRouter);
