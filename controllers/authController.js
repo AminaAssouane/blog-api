53097b94729755992a8dc67910e0a4383b2cc9e5
Amina <minassouane@gmail.com>
2026-03-09 15:59:03 +0100

    feat(auth): implement login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,27 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const db = require("../lib/queries");
 
-function login(req, res) {}
+async function login(req, res) {
+  const { username, password } = req.body;
+
+  const user = await db.getUserByUsername(username);
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  res.json({ token });
+}
 
 module.exports = { login };
